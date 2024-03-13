0abcea0ad281491fa06ae1b9d83660c2a3108c41
VyacheslavKuchumov <Kuchumov76@mail.ru>
2024-03-13 14:31:52 +0500

    Update index.js

---
+++ front/src/middlewares/index.js
@@ -0,0 +1,16 @@
+import axios from "axios"
+import store from "@/store"
+
+const instance = axios.create({
+    baseURL: process.env.VUE_APP_SERVER,
+    headers:{
+        'Content-Type': 'application/json; charset=utf-8'
+    }
+})
+
+instance.interceptors.request.use(
+    function (config){
+        config.headers['x-access-token'] = localStorage.getItem('accesToken')
+        return config
+    }
+)
